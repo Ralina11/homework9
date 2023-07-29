591f3fe2a4f94d4cc34629d5b07fec1835823f35
Ralina Latypova <Latypovaralina99@gmail.com>
2023-07-28 23:40:13 -0700

    chance FBV on the CBV

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index, categories, one_product
+from main.views import ProductsListView, CategoryListView, OneProductDetailView
 
 app_name = MainConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('categories/', categories, name='categories'),
-    path('<int:pk>/product/', one_product, name='one_product'),
+    path('', ProductsListView.as_view(), name='index_list'),
+    path('categories/', CategoryListView.as_view(), name='categories_list'),
+    path('<int:pk>/product/', OneProductDetailView.as_view(), name='product_detail'),
  ]
--- main/views.py
+++ main/views.py
@@ -1,29 +1,69 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from main.models import Category, Product
 
 
 # Create your views here.
-def index(request):
+
+class ProductsListView(ListView):
     """Контроллер страницы со всеми товарами"""
-    context = {
-        'object_list': Product.objects.all(),
+    model = Product
+    template_name = 'main/index_list.html'
+    context_object_name = 'object_list'
+    extra_context = {
         'title': 'Товары'
     }
-    return render(request, 'main/index.html', context)
+# def index(request):
+#     """Контроллер страницы со всеми товарами"""
+#     context = {
+#         'object_list': Product.objects.all(),
+#         'title': 'Товары'
+#     }
+#     return render(request, 'main/index_list.html', context)
 
-def categories(request):
+# def categories(request):
+#     """Контроллер страницы со списком категорий"""
+#     context = {
+#         'object_list': Category.objects.all(),
+#         'title': 'Категории'
+#     }
+#     return render(request, 'main/categories_list.html', context)
+
+class CategoryListView(ListView):
     """Контроллер страницы со списком категорий"""
-    context = {
-        'object_list': Category.objects.all(),
-        'title': 'Категории'
+    model = Category
+    template_name = 'main/categories_list.html'
+    context_object_name = 'categories_list'
+    extra_context = {
+        'title': 'Категории товаров'
     }
-    return render(request, 'main/categories.html', context)
 
-def one_product(request, pk):
-    product_item = Product.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(id=pk),
-        'title': f'Продукт {product_item.name}'
-    }
-    return render(request, 'main/product.html', context)
+
+
+# def one_product(request, pk):
+#     """Контроллер страницы с одним товаром"""
+#     product_item = Product.objects.get(pk=pk)
+#     context = {
+#         'object_list': Product.objects.filter(id=pk),
+#         'title': f'Продукт {product_item.name}'
+#     }
+#     return render(request, 'main/product_detail.html', context)
+
+class OneProductDetailView(DetailView):
+    model = Product
+    template_name = 'main/product_detail.html'
+    context_object_name = 'object'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = Product.objects.filter(id=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        product_item = Product.objects.get(pk=self.kwargs.get('pk'))
+        context_data['product_pk'] = product_item.pk,
+        context_data['title'] = f'  {product_item.name}'
+
+        return context_data
